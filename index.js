755ca846fd588707fc0d7d6485dc7abb209fe32d
Damon-Nunez <damon.nunez@student.uncommonhs.org>
2021-10-26 14:40:48 +0000

    Answers

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
-//1
+
 function countToTwenty(){
-  
+  for(let i=1;i <= 20;i++){
+      console.log(i);
+  }
 }
 
-//2
+
 function countFromTwenty(){
-  
+  for(let i=20;i >= 1; i--){
+      console.log(i);
+  }
 }
 
 //3
-function countToN(){
-  
+function countToN(num){
+  for(let i=0;i <= num; i++){
+      console.log(i);
+  }
 }
 
 
 //4
-function countFromN(){
-  
+function countFromN(num){
+  for(let i = num; i >= 1; i--){
+      console.log(i)
+  }
 }
 
 //5
-function countEveryOdd(){
-  
+function countEveryOdd(num){
+  for(let i=0; i <= num; i++){
+      if(i % 2 === 0){
+         
+      }else {
+          console.log(i)
+      }
+  }
 }
+
+
+
+
+countEveryOdd(9)
